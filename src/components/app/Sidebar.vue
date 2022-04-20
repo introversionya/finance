8f9838150a8637155e0fb78a4ca3b19ea8317d29
Anton Demidenko <yaintroversivnost@gmail.com>
2022-04-20 07:48:29 +0500

    first commit

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      :exact="link.exact"
+      custom
+      v-slot="{ href, navigate, isActive }"
+    >
+      <li :class="[isActive && 'active']" :active="isActive">
+        <a
+          :href="href"
+          @click="navigate"
+          class="waves-effect waves-orange pointer"
+        >
+          {{ link.title }}
+        </a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
+</script>
+
+
+
+
+
+
